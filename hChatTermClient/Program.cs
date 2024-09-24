8efd967aeb0b51d42aca9ca4a3bd0ae2f2ece499
Vasco Teixeira <pg55096@uminho.pt>
2024-09-24 10:58:28 +0100

    Test

--- hChatTermClient/Program.cs
+++ hChatTermClient/Program.cs
@@ -1,18 +1,20 @@
 ﻿
 using hChatShared;
+using hChatShared.Net;
 using hChatShared.Services;
+using hChatTermClient.Wrapper;
 
 namespace hChatTermClient {
 	public class Program {
 		static async Task Main(string[] args) {
 			Utils.CreateAppDataFolder();
-			// var client = new Client();
-			// await client.Run();
-			// var packet = new PacketBuilder();
-			// packet.WriteOpCode(0x03);
-			// packet.WriteMessage("admin");
-			// packet.WriteMessage("Mensagem random pra teste");
-			// await client.Send(packet);
+			var client = new Client();
+			await client.Run();
+			var packet = new PacketBuilder();
+			packet.WriteOpCode(0x03);
+			packet.WriteMessage("admin");
+			packet.WriteMessage("Mensagem random pra teste");
+			await client.Send(packet);
 			
 			Console.WriteLine(EncryptedCommunicationHandler.ExportPublicKey());
 			Console.ReadKey();
